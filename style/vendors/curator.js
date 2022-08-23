cc3b35aec555462b1d535261983387533a5aaa74
Laetitia Taddei <laetdi@gmail.com>
2022-08-23 09:44:04 +0200

    curator.js in individual file

---
+++ style/vendors/curator.js
@@ -0,0 +1,5 @@
+(function () {
+      var i, e, d = document, s = "script"; i = d.createElement("script"); i.async = 1; i.charset = "UTF-8";
+      i.src = "https://cdn.curator.io/published/9b34b0e7-c3c5-4cf8-be43-e346321f3280.js";
+      e = d.getElementsByTagName(s)[0]; e.parentNode.insertBefore(i, e);
+    })();
