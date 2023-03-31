e2f3dfd9b493911b59d9b1262ef6c5bd6aa6260b
Laetitia Taddei <laetdi@gmail.com>
2023-03-31 14:33:13 +0200

    fetch api instagram

--- style/vendors/basicDisplay.js
+++ style/vendors/basicDisplay.js
@@ -1,1 +1,33 @@
-var token = config.TOKEN
+var access_token = config.access_token
+var user_id = config.user_id
+const instaUrl = `https://graph.instagram.com/me/media?fields=id,media_url,media_type,timestamp&access_token=${access_token}`
+let items = []
+
+// fetch(instaUrl)
+//   .then(response => {
+//     // indicates whether the response is successful (status code 200-299) or not
+//     if (!response.ok) {
+//       throw new Error(`Request failed with status ${response.status}`)
+//     }
+//     return response.json()
+//   }).then(data => {
+//     return data.data[0]
+//   })
+
+const datas = async(url) => {
+  await fetch(url)
+  .then((response) => {
+    return response.json()
+  }).then((data) => {
+    // console.log(data.data)
+    items.push(data.data)
+    return items
+  })
+}
+// console.log(items)
+const elem = datas(instaUrl)
+
+console.log(items)
+// items.map(item => {
+//   console.log(item)
+// })
--- style/vendors/insta.js
+++
@@ -1,5 +0,0 @@
-async () => {
-    const res = await fetch('https://graph.instagram.com/{user-id}?fields=id,username&access_token={access-token}')
-    const answer = await res.json()
-    console.log(answer)
-}
