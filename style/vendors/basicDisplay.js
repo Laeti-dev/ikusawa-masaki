2f281341033c46e7c4e33075d7e32ef2ec6e0882
Laetitia Taddei <laetdi@gmail.com>
2023-04-01 13:16:41 +0200

    get an empty array but all my objects are following

--- style/vendors/basicDisplay.js
+++ style/vendors/basicDisplay.js
@@ -1,33 +1,55 @@
 var access_token = config.access_token
 var user_id = config.user_id
 const instaUrl = `https://graph.instagram.com/me/media?fields=id,media_url,media_type,timestamp&access_token=${access_token}`
-let items = []
 
-// fetch(instaUrl)
-//   .then(response => {
-//     // indicates whether the response is successful (status code 200-299) or not
+
+// const datas = async(url) => {
+//   await fetch(url)
+//   .then((response) => {
 //     if (!response.ok) {
 //       throw new Error(`Request failed with status ${response.status}`)
-//     }
+//     };
 //     return response.json()
-//   }).then(data => {
-//     return data.data[0]
+//   }).then((data) => {
+//     for (let i = 0; i < data.data.length; i++) {
+//         items.push(data.data[i]);
+//       }
+//     return(items)
 //   })
+// }
 
-const datas = async(url) => {
-  await fetch(url)
-  .then((response) => {
-    return response.json()
-  }).then((data) => {
-    // console.log(data.data)
-    items.push(data.data)
-    return items
+
+const datas = async() => {
+  let items = []
+  await fetch(instaUrl)
+  .then(response => response.json())
+  .then((data) => {
+    for(let i=0; i<data.data.length; i++){
+      items.push(data.data[i])
+      // console.log(items)
+      // return items
+    }
   })
+  return items
 }
-// console.log(items)
-const elem = datas(instaUrl)
+values = datas();
+array = []
 
-console.log(items)
+values.then(value=>{
+  for(let i=0; i<value.length; i++){
+      array.push(value[i])
+      }})
+
+console.log(array)
+// const datas = async() => {
+//   await fetch(instaUrl)
+//   .then(response => response.json())
+//   .then(data=> console.log(data.data))
+//   .catch ((err) => console.log(err))
+// }
+
+
+
 // items.map(item => {
 //   console.log(item)
 // })
